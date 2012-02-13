a7c23e9a4a5425ee177628ae6d44b24c5a308831
Nickolay Mazurkin <mazurkin@gmail.com>
2012-02-13 12:55:50 +0400

    Minor changes

--- lib-model/src/main/java/liquibase/spring/LiquibaseInitializer.java
+++ lib-model/src/main/java/liquibase/spring/LiquibaseInitializer.java
@@ -3,16 +3,25 @@ package liquibase.spring;
 import liquibase.Liquibase;
 import liquibase.exception.LiquibaseException;
 import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LiquibaseInitializer extends SpringLiquibase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LiquibaseInitializer.class);
 
     private boolean enabled;
 
     @Override
     public void afterPropertiesSet() throws LiquibaseException {
+        LOGGER.info("Liquibase update is {}", enabled ? "active" : "disabled");
+
         System.setProperty(Liquibase.SHOULD_RUN_SYSTEM_PROPERTY, Boolean.toString(enabled));
-        super.afterPropertiesSet();
-        System.getProperties().remove(Liquibase.SHOULD_RUN_SYSTEM_PROPERTY);
+        try {
+            super.afterPropertiesSet();
+        } finally {
+            System.getProperties().remove(Liquibase.SHOULD_RUN_SYSTEM_PROPERTY);
+        }
     }
 
     public void setEnabled(boolean enabled) {
