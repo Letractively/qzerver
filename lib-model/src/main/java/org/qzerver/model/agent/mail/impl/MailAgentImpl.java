58deffe755a0e0aaa85f1d7ff95779243f4e037b
Nickolay Mazurkin <mazurkin@gmail.com>
2012-05-15 13:29:39 +0400

    Mailer logging

--- lib-model/src/main/java/org/qzerver/model/agent/mail/impl/MailAgentImpl.java
+++ lib-model/src/main/java/org/qzerver/model/agent/mail/impl/MailAgentImpl.java
@@ -3,6 +3,8 @@ package org.qzerver.model.agent.mail.impl;
 import org.hibernate.validator.constraints.Email;
 import org.qzerver.model.agent.mail.MailAgent;
 import org.qzerver.model.agent.mail.MailAgentException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -11,6 +13,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import javax.validation.constraints.NotNull;
 
 public class MailAgentImpl implements MailAgent {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MailAgentImpl.class);
 
     @NotNull
     private JavaMailSender javaMailSender;
@@ -21,6 +25,8 @@ public class MailAgentImpl implements MailAgent {
 
     @Override
     public void sendMail(String emailTo, String subject, String text) throws MailAgentException {
+        LOGGER.debug("Sending message to [{}] with subject [{}]", emailTo, subject);
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(emailFrom);
         message.setTo(emailTo);
